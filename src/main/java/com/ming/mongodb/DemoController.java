d728ee05fbf5d8b9a4bea8f525ef8fde313dc1c8
chiminwon <chiminwon@gmail.com>
2017-08-07 17:11:48 +0800

    Initial commit

---
+++ src/main/java/com/ming/mongodb/DemoController.java
@@ -0,0 +1,49 @@
+package com.ming.mongodb;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+  
+   @Autowired
+   private DemoInfoRepository demoInfoRepository;
+   
+   @Autowired
+   private MongoTemplate mongoTemplate;
+  
+   @RequestMapping("save")
+   public String save(){
+      DemoInfo demoInfo = new DemoInfo();
+      demoInfo.setName("张三");
+      demoInfo.setAge(20);
+      demoInfoRepository.save(demoInfo);
+     
+      demoInfo = new DemoInfo();
+      demoInfo.setName("李四");
+      demoInfo.setAge(30);
+      demoInfoRepository.save(demoInfo);
+     
+      return "ok";
+   }
+  
+   @RequestMapping("find")
+   public List<DemoInfo> find(){
+      return demoInfoRepository.findAll();
+   }
+  
+   @RequestMapping("findByName")
+   public DemoInfo findByName(){
+      return demoInfoRepository.findByName("张三");
+   }
+   
+   @RequestMapping("find2")
+   public List<DemoInfo> find2(){
+      return mongoTemplate.findAll(DemoInfo.class);
+   }
+  
+}
